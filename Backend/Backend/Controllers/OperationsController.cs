db22344026e9fbd34a51389ee26ed00f3b1a4fbf
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-10-11 14:01:46 -0600

    Guía VI - Cuerpo de solicitud HTTP (finalizada)

--- Backend/Backend/Controllers/OperationsController.cs
+++ Backend/Backend/Controllers/OperationsController.cs
@@ -16,9 +16,9 @@ namespace Backend.Controllers
 
         [HttpPost]
 
-        public decimal Add(decimal a, decimal b)
+        public decimal Add(Numbers c)
         {
-            return a - b;
+            return c.A - c.B;
         }
 
         [HttpPut]
--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60cead92a70f627e8b1be3f08bf793c9ba4f23d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da8b8c284c74ed42b589025029d7b155725fc9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
