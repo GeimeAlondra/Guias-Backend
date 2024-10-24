8c733f902cdc2607197246b08fe77ff315b65319
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-10-23 21:46:59 -0600

    Guía XII - Programación asíncrona (finalizada)

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0772754ff7a5ee44de49227aaac1341c620f6f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb8bd6816c70fe65c9aeb6db66e97e9df06c339")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
