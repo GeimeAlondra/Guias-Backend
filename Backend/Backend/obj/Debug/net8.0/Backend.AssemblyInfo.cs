cea6e22244b083d345f1c0bfe93789ffbba815ff
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-10-23 22:18:21 -0600

    Guía XV - Repaso Entity Framework  (finalizada)

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc38417c6b4d140c396f68b18a6787e2a159bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91dba7652bc472bd48694e45b8d04c412f87345")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
