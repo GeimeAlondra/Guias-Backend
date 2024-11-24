5446296c88c181b97f72e3461ffaae1acb7cb64b
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-11-23 20:19:00 -0600

    Guía XVII - Creación de DTO (finalizada)

---
+++ Backend/Backend/Controllers/BeerController.cs
@@ -0,0 +1,40 @@
+﻿using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private StoreContext _storeContext;
+
+        public BeerController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BeerDto>> GetById(int id) {
+    
+            var beer = await _storeContext.Beers.FindAsync(id);
+            if (beer == null)
+            {
+                return NotFound();
+            }
+
+            var beerDto = new BeerDto
+            {
+                Id = beer.BrandId,
+                Al = beer.Al,
+                BrandID = beer.BrandId,
+                Name = beer.BeerName
+            };
+            return Ok(beerDto);
+        }
+    }
+}
--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea6e22244b083d345f1c0bfe93789ffbba815ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11a2b5874d02701cbe901feba31914e02c24e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
